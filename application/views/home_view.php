647540911bbfd3cc347b24c5244ab3c16086fb8b
Minja Junior <mailgiven@gmail.com>
2019-07-16 00:16:16 +0300

    Home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,12 +7,12 @@
                     <div class="row">
                         <div class="col">
                             <div class="main-banner-content">
-                                <h1 class="main-banner-title anim fadeInUp">IT Solutions for You Business</h1>
+                                <h1 class="main-banner-title anim fadeInUp">Aluminium Profile</h1>
                                 <div class="main-banner-desc anim fadeInUp" data-wow-delay="0.2s">
-                                    <p>On this template you will be able to create a site for your business. The template has many different blocks from which you can create your own unique site.</p>
+                                    <p>All sorts of Aluminium profile and Aluminium accessories. Available in wholesale and retail.</p>
                                 </div>
                                 <div class="main-banner-btns anim fadeInUp" data-wow-delay="0.3s">
-                                    <a href="#!" class="btn btn-widht-ico btn-small ripple anim fadeInLeft" data-wow-delay="0.4s">
+                                    <a href="<?php echo base_url('product')?>" class="btn btn-widht-ico btn-small ripple anim fadeInLeft" data-wow-delay="0.4s">
                                         <span>Learn More</span>
                                         <svg class="btn-widht-ico-right" viewBox="0 0 13 9"><use xlink:href="<?php echo base_url('assets/images/sprite.svg#arrow-right')?>"></use></svg>
                                     </a>
@@ -22,37 +22,17 @@
                     </div>
                 </div>
             </div>
-            <div class="main-banner-slider-item lazy" data-src="<?php echo base_url('assets/images/aluminium-profile.jpg')?>">
+            <div class="main-banner-slider-item lazy" data-src="<?php echo base_url('assets/images/curtains.jpg')?>">
                 <div class="container">
                     <div class="row">
                         <div class="col">
                             <div class="main-banner-content">
-                                <h2 class="main-banner-title anim fadeInUp">The Best HTML5 Template in 2019</h2>
+                                <h2 class="main-banner-title anim fadeInUp">Curtains</h2>
                                 <div class="main-banner-desc anim fadeInUp" data-wow-delay="0.2s">
-                                    <p>On this template you will be able to create a site for your business. The template has many different blocks from which you can create your own unique site.</p>
+                                    <p>We offer different types of curtains, blinds and curtain accessories based on our customers’ requirements.  Available in wholesale and retail.</p>
                                 </div>
                                 <div class="main-banner-btns anim fadeInUp" data-wow-delay="0.3s">
-                                    <a href="#!" class="btn btn-widht-ico btn-small ripple anim fadeInLeft" data-wow-delay="0.4s">
-                                        <span>Learn More</span>
-                                        <svg class="btn-widht-ico-right" viewBox="0 0 13 9"><use xlink:href="<?php echo base_url('assets/images/sprite.svg#arrow-right')?>"></use></svg>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="main-banner-slider-item lazy" data-src="<?php echo base_url('assets/images/aluminium-profile.jpg')?>">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <div class="main-banner-content">
-                                <h2 class="main-banner-title anim fadeInUp">IT Solutions for You Business</h2>
-                                <div class="main-banner-desc anim fadeInUp" data-wow-delay="0.2s">
-                                    <p>On this template you will be able to create a site for your business. The template has many different blocks from which you can create your own unique site.</p>
-                                </div>
-                                <div class="main-banner-btns anim fadeInUp" data-wow-delay="0.3s">
-                                    <a href="#!" class="btn btn-widht-ico btn-small ripple anim fadeInLeft" data-wow-delay="0.4s">
+                                    <a href="<?php echo base_url('product')?>" class="btn btn-widht-ico btn-small ripple anim fadeInLeft" data-wow-delay="0.4s">
                                         <span>Learn More</span>
                                         <svg class="btn-widht-ico-right" viewBox="0 0 13 9"><use xlink:href="<?php echo base_url('assets/images/sprite.svg#arrow-right')?>"></use></svg>
                                     </a>
